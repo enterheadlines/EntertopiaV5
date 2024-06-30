85d32dff0abdc954a58ade8d07e2185cc840ff91
enterheadlines <162852023+enterheadlines@users.noreply.github.com>
2024-06-30 15:05:32 +0900

    9;.;hi;.

--- searchingstuffidfk.js
+++
@@ -1,7 +0,0 @@
-
-  document.getElementById("search").addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault(); // Prevent the default form submission
-      document.getElementById("searc").submit(); // Manually submit the form
-    }
-  });
